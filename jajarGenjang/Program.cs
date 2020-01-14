dc831123d14d6a987719694e243bc5e291ec6d9a
Muhammad Barokah <m.nurhadbarokah@gmail.com>
2020-01-14 21:50:07 +0700

    Add files via upload

---
+++ jajarGenjang/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace jajarGenjang
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 10; i > 0; i--)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int k = 0; k < 20; k++)
+                {
+                    Console.Write("#");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
